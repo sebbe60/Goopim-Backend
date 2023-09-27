1d315100c40d90c0ad6ca2857ae8242941480798
saadSarwar28 <saad_sarwar22@hotmail.com>
2023-09-27 20:47:04 +0500

    api route changed for testing

--- app/app.py
+++ app/app.py
@@ -2763,7 +2763,7 @@ def vector_db_status():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.route('/health', methods=["GET"])
+@app.route('api/health', methods=["GET"])
 def health_check():
     return jsonify({"result": "deployment complete, second test"})
 
