c81edb04497720d1e9d239328a36f4d49083a5f5
saadSarwar28 <saad_sarwar22@hotmail.com>
2023-09-27 15:50:41 +0500

    testing

--- app/app.py
+++ app/app.py
@@ -2765,7 +2765,7 @@ def vector_db_status():
 
 @app.route('/health', methods=["GET"])
 def health_check():
-    return jsonify({"result": "hi there, development complete"})
+    return jsonify({"result": "hi there, development testing"})
 
 
 if __name__ == "__main__":
