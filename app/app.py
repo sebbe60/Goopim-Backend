6f45ff62bef05acc63d72770672feb9586902bbd
saadSarwar28 <saad_sarwar22@hotmail.com>
2023-09-27 15:44:43 +0500

    files in the wrong folder deleted

--- app/app.py
+++ app/app.py
@@ -2765,7 +2765,7 @@ def vector_db_status():
 
 @app.route('/health', methods=["GET"])
 def health_check():
-    return jsonify({"result": "hi there"})
+    return jsonify({"result": "hi there, development complete"})
 
 
 if __name__ == "__main__":
